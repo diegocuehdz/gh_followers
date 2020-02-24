0b65bd7d65ab331a4170db00f216d3c6868a626e
Diego Cué <diegocuehdz@gmail.cm>
2020-02-23 23:23:58 -0600

    Se agregan los archivos de containers.
    Signed-off-by: Diego Cué <diegocuehdz@gmail.cm>

---
+++ GHFollowers/ViewControllerss/Containers/FavoritesListVC/FavoritesListVC.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesListVC.swift
+//  GHFollowers
+//
+//  Created by Diego Cué Hernández on 23/02/20.
+//  Copyright © 2020 Diego Cué Hernández. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+}
---
+++ GHFollowers/ViewControllerss/Containers/SearchVC/SearchVC.swift
@@ -0,0 +1,19 @@
+//
+//  SearchVC.swift
+//  GHFollowers
+//
+//  Created by Diego Cué Hernández on 23/02/20.
+//  Copyright © 2020 Diego Cué Hernández. All rights reserved.
+//
+
+import UIKit
+
+class SearchVC: UIViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemRed
+    }
+}
