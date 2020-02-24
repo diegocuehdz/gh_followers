8a4758f55c05047a18a5637d8a80f873422fd3f9
Diego Cué <diegocuehdz@gmail.cm>
2020-02-24 00:09:23 -0600

    Se mete el boton de
    Signed-off-by: Diego Cué <diegocuehdz@gmail.cm>

---
+++ GHFollowers/CustomViews/GFTextField/GFTextField.swift
@@ -0,0 +1,45 @@
+//
+//  GFTextField.swift
+//  GHFollowers
+//
+//  Created by Diego Cué Hernández on 24/02/20.
+//  Copyright © 2020 Diego Cué Hernández. All rights reserved.
+//
+
+import UIKit
+
+class GFTextField: UITextField
+{
+    //https://github.com/aaronbrethorst/SemanticUI
+    override init(frame: CGRect)
+    {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder)
+    {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure()
+    {
+        translatesAutoresizingMaskIntoConstraints = false
+
+        layer.cornerRadius = 10
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.systemGray4.cgColor
+        
+        textColor = .label
+        tintColor = .label
+        textAlignment = .center
+        font = UIFont.preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth = true
+        minimumFontSize = 12
+        
+        backgroundColor = .tertiarySystemBackground
+        autocorrectionType = .no
+        
+        placeholder = "Enter a username"
+    }
+}
