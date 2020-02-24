47d0b02b34d73786b2b9278831599a0005866f50
Diego Cué <diegocuehdz@gmail.cm>
2020-02-23 23:35:56 -0600

    Se modifica el tabbar.
    Signed-off-by: Diego Cué <diegocuehdz@gmail.cm>

--- GHFollowers/SceneDelegate.swift
+++ GHFollowers/SceneDelegate.swift
@@ -15,12 +15,42 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions)
     {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        
+                
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
-        window?.rootViewController = UITabBarController()
+        window?.rootViewController = createTabBar()
         window?.makeKeyAndVisible()
     }
+    
+    func createTabBar() -> UITabBarController
+    {
+        let tabBar = UITabBarController()
+        UITabBar.appearance().tintColor = .systemGreen
+        
+        tabBar.viewControllers = [createSearchNC(), createFavoritesNC()]
+
+        return tabBar
+    }
+    
+    func createSearchNC() -> UINavigationController
+    {
+        let searchVC = SearchVC()
+        searchVC.title = "Search"
+        searchVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
+        
+        return UINavigationController(rootViewController: searchVC)
+    }
+    
+    func createFavoritesNC() -> UINavigationController
+    {
+        let favoritesVC = FavoritesListVC()
+        favoritesVC.title = "Favorites"
+        favoritesVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 0)
+
+        
+        return UINavigationController(rootViewController: favoritesVC)
+    }
+    
 
     func sceneDidDisconnect(_ scene: UIScene)
     {
