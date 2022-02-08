09967058d44c45813b17f24543d1d346d6b23518
Diego Cué <diego@tr3sco.com>
2022-02-07 19:48:58 -0600

    FlowLayout.
    Signed-off-by: Diego Cué <diego@tr3sco.com>

--- GHFollowers/VCs/Containers/FollowerListVC/FollowerListVC.swift
+++ GHFollowers/VCs/Containers/FollowerListVC/FollowerListVC.swift
@@ -11,12 +11,27 @@ import UIKit
 class FollowerListVC: UIViewController {
 
     var userName: String = ""
+    var collectionView_Followers: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configViewController()
+        configCollectionView()
+        loadData()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    
+    
+    func configViewController() {
         view.backgroundColor = .systemBackground
         self.navigationController?.navigationBar.prefersLargeTitles = true
-        
+    }
+    
+    func loadData() {
         NetworkManager.shared.getFollowers(for: userName, page: 1) { result in
             switch result {
             case .failure(let error ):
@@ -27,8 +42,24 @@ class FollowerListVC: UIViewController {
         }
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        navigationController?.setNavigationBarHidden(false, animated: true)
+    func configCollectionView() {
+        self.collectionView_Followers = UICollectionView(frame: view.bounds, collectionViewLayout: createCollectionViewFlowLayout())
+        view.addSubview(collectionView_Followers)
+        collectionView_Followers.backgroundColor = .systemPink
+        collectionView_Followers.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.kIdentifier)
     }
+    
+    func createCollectionViewFlowLayout() -> UICollectionViewFlowLayout {
+        let width = view.bounds.width
+        let padding: CGFloat = 12
+        let minimumItemSpacing: CGFloat = 10
+        let numItemsPerRow: CGFloat = 3.0
+        let availableWidth = width - (padding * 2) - (minimumItemSpacing * 2)
+        let widthPerItem = availableWidth/numItemsPerRow
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize = CGSize(width: widthPerItem, height: widthPerItem + 40)
+        return flowLayout
+    }
+    
 }
