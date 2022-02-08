9915fc33045c34045742ee953717a128c4bef88c
Diego Cué <diego@tr3sco.com>
2022-02-07 19:19:19 -0600

    Cabmios de Errores y capa de peticiones.
    Signed-off-by: Diego Cué <diego@tr3sco.com>

---
+++ GHFollowers/Models/User.swift
@@ -0,0 +1,24 @@
+//
+//  User.swift
+//  GHFollowers
+//
+//  Created by Diego Cué Hernández on 07/02/22.
+//  Copyright © 2022 Diego Cué Hernández. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable
+{
+    var login: String
+    var avatarUrl: String
+    var name: String?
+    var location: String?
+    var bio: String?
+    var publicRepos: Int
+    var pulbicGists: Int
+    var htmlUrl: String
+    var followers: Int
+    var following: Int
+    var createdAt: String
+}
---
+++ GHFollowers/NetworkManager/NetworkManager.swift
@@ -0,0 +1,56 @@
+//
+//  NetworkManager.swift
+//  GHFollowers
+//
+//  Created by Diego Cué Hernández on 07/02/22.
+//  Copyright © 2022 Diego Cué Hernández. All rights reserved.
+//
+
+import Foundation
+
+class NetworkManager
+{
+    static let shared = NetworkManager()
+    private let baseURL = "https://api.github.com/users"
+
+    private init() { }
+    
+    func getFollowers(for username: String, page: Int, handler: @escaping (Result<[Follower], GFError>) -> Void) {
+        let endpoint = baseURL + "/\(username)/followers?per_page=100&page=\(page)"
+        guard let url = URL(string: endpoint) else {
+            handler(.failure(.errorInvalidUrl))
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let _ = error {
+                handler(.failure(.errorUnableToComplete))
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                handler(.failure(.errorInvalidResponse))
+                return
+            }
+            
+            guard let data = data else {
+                handler(.failure(.errorInvalidResponse))
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                let followers = try decoder.decode([Follower].self, from: data)
+                handler(.success(followers))
+                return
+            } catch {
+                handler(.failure(.errorParsingData))
+                return
+            }
+        }
+
+        task.resume()
+    }
+}
+
