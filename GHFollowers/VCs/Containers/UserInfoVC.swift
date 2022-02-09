ae37335b0fe75b6b738c2a795a51812c7d768c2a
Diego Cué <diego@tr3sco.com>
2022-02-08 22:47:08 -0600

    Cambio a parseos y a modelos.
    Signed-off-by: Diego Cué <diego@tr3sco.com>

---
+++ GHFollowers/VCs/Containers/UserInfoVC.swift
@@ -0,0 +1,30 @@
+//
+//  UserInfoVC.swift
+//  GHFollowers
+//
+//  Created by Diego Cué Hernández on 08/02/22.
+//  Copyright © 2022 Diego Cué Hernández. All rights reserved.
+//
+
+import UIKit
+
+class UserInfoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
